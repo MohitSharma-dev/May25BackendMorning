f74612a2d812bd309213816530ec886a5e92e2b6
Bharadwaj <bharadwajreddy9@gmail.com>
2025-08-06 08:12:33 +0530

    row winning update

--- src/main/java/TicTacToe/strategies/RowWinningStrategy.java
+++ src/main/java/TicTacToe/strategies/RowWinningStrategy.java
@@ -12,17 +12,17 @@ public class RowWinningStrategy implements WinningStrategy {
     // we maintain a map to keep track of the count of symbols in each row
     // key is the row number, value is a map of symbols and their counts in that row
     // if a symbol's count reaches the size of the board, we have a winner
-    private final Map<Integer, Map<Symbol, Integer>> rowWinner = new HashMap<>();
+    private final Map<Integer, Map<String, Integer>> rowWinner = new HashMap<>();
     @Override
     public boolean checkWinner(Board board, Move move) {
 
         Cell cell= move.getCell();
-        Symbol symbol = move.getPlayer().getSymbol();
+        String symbol = move.getPlayer().getCharacter();
         int row = cell.getRow();
         if(!rowWinner.containsKey(row)) {
             rowWinner.put(row, new HashMap<>());
         }
-        Map<Symbol, Integer> symbolCount = rowWinner.get(row);
+        Map<String, Integer> symbolCount = rowWinner.get(row);
         symbolCount.put(symbol, symbolCount.getOrDefault(symbol, 0) + 1);
         if(symbolCount.get(symbol) == board.getSize()) {
             return true;
